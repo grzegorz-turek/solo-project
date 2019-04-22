40af67688a978b92f11447f6732117ea45343766
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-04-22 23:40:33 +0200

    Fix minor errors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-// Get the container element
 var btnContainer = document.getElementById('menuLinkPrmanentHover');
-
-// Get all buttons with class='btn' inside the container
 var btns = btnContainer.getElementsByClassName('menu--link--textdecoration-none');
-
-// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function() {
     var current = document.getElementsByClassName('active');
