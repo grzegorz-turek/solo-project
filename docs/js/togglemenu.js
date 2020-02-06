8e898a1d6df43e1265a8fbaa9b28c3817a294fbd
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2020-02-06 23:37:48 +0100

    Fixed Lighthouse optimisation bug

--- docs/js/togglemenu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(visible) {
-    document.querySelector('.menu__link__container').classList.toggle('show', visible);
-}
-
-document.querySelector('.menu__hamburger').addEventListener('click', function(e) {
-    e.preventDefault();
-    toggleMenu();
-});
