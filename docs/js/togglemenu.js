c78fbe2e2a8560747f3f9cc588d60802ad98d680
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2020-02-06 16:33:53 +0100

    Fixed graph auto-width v.2

---
+++ docs/js/togglemenu.js
@@ -0,0 +1,8 @@
+function toggleMenu(visible) {
+    document.querySelector('.menu__link__container').classList.toggle('show', visible);
+}
+
+document.querySelector('.menu__hamburger').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleMenu();
+});
